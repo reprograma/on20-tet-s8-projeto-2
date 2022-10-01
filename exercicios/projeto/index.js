a9ce5b2afc7e0e70a96131dfc1ef2d8dcf799868
Ana Beatriz Morita <anabemorita@gmail.com>
2022-10-01 19:42:34 -0300

    Limpar campo do input ao cadastrar uma tarefa

--- exercicios/projeto/index.js
+++ exercicios/projeto/index.js
@@ -42,6 +42,7 @@ function cadastrarTarefa(evento) {
   } else {
     let itemDaLista = document.createElement('li');
     itemDaLista.innerText = inputTarefa.value;
+    inputTarefa.value = '';
 
     let botaoDeletar = document.createElement('button');
     itemDaLista.appendChild(botaoDeletar);

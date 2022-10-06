6adb15b4fdfde479b84d77cc17d5d30c5df6f141
Rai Alcides <railayli2@hotmail.com>
2022-10-05 21:49:49 -0300

    marcar e remover elementos

--- exercicios/projeto/index.js
+++ exercicios/projeto/index.js
@@ -2,10 +2,28 @@ let inputTafera = document.querySelector('form input');
 let form = document.querySelector('form')
 let ul = document.getElementById('cadastrar-list')
 
-function cadastarTarefa(evento){
+function marcarTarefa(evento) {
+   evento.target.classList.toggle('check')
+   console.log(evento)
+}
+
+function deletarTarefa(evento) {
+    ul.removeChild(evento.target.parentElement)
+}
+
+
+function cadastarTarefa(evento) {
     evento.preventDefault()
+
+
    let li = document.createElement('li') 
    li.innerText = inputTafera.value 
+
+   let = botaoDeletar = document.createElement('button')
+   li.appendChild(botaoDeletar)
+   botaoDeletar.addEventListener('click', deletarTarefa)
+
+   li.addEventListener('click', marcarTarefa)
    ul.appendChild(li)
 }
 

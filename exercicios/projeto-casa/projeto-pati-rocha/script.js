d7a6dc4120d3a144a9b4230076bc5dcc43bb929a
Pati Rocha <patirocha.dev@gmail.com>
2022-10-06 18:32:31 -0300

    Adicionada funcionalidade marcar tarefa

--- exercicios/projeto-casa/projeto-pati-rocha/script.js
+++ exercicios/projeto-casa/projeto-pati-rocha/script.js
@@ -4,11 +4,18 @@ let form = document.querySelector('form');
 let btnForm = document.querySelector('form button');
 let ulLista = document.querySelector('ul');
 
+
+function marcarTarefa(e) {
+    e.target.classList.toggle('marcado')
+}
+
+
 function adicionarTarefa(e) {
     e.preventDefault();
     let itemDaLista = document.createElement('li');
     itemDaLista.innerHTML = input.value;
     ulLista.appendChild(itemDaLista)    
+    itemDaLista.addEventListener('click', marcarTarefa)
 }
 form.addEventListener('submit', adicionarTarefa);
 

67441b6599a1889072efa60b5836e5a1bc9af4ec
Adriana Félix da Silva <adrianafelix.cg@hotmail.com>
2022-10-06 16:19:45 -0300

    Tentativa de finalização de progeto guiado

---
+++ exercicios/projeto/index.js
@@ -0,0 +1,22 @@
+let inputTarefa = document.querySelector('form input');
+let form = document.querySelector('form');
+let ul = document.getElementById('todo-list-container');
+// PEGAR O QUE ESTA ESCRITO NO INPUT
+// console.log(inputTarefa.value)
+
+function marcarTarefa(evento) {
+  evento.target.classList.toggle('checked');
+}
+
+function cadastrarTarefa(evento) {
+  evento.preventDefault();
+
+  let itemDaLista = document.createElement('li');
+  itemDaLista.innerText = inputTarefa.value;
+  // elementoPai.appendChild(elementoFilho) -> elementoFilho é o elemento a ser adicionado
+  itemDaLista.addEventListener('click', marcarTarefa)
+  ul.appendChild(itemDaLista);
+}
+
+// FORM -> SUBMIT (clicar no botao do form ou apertar o enter dentro input) -> cadastrarTarefa(evento)
+form.addEventListener('submit', cadastrarTarefa);

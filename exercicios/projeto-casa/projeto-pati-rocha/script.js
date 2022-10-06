5ada85cc013249b5f8aa855242ab8002f57e0e71
Pati Rocha <patirocha.dev@gmail.com>
2022-10-06 19:35:58 -0300

    Funcionalidade apagar uma tarefa da lista

--- exercicios/projeto-casa/projeto-pati-rocha/script.js
+++ exercicios/projeto-casa/projeto-pati-rocha/script.js
@@ -6,16 +6,30 @@ let ulLista = document.querySelector('ul');
 
 
 function marcarTarefa(e) {
-    e.target.classList.toggle('marcado')
+    //e.target é o elemento que recebeu o evento
+    e.target.classList.toggle('marcado');
 }
-
+function deletarTarefa(e) {
+   ulLista.removeChild(e.target.parentElement);
+}
 
 function adicionarTarefa(e) {
     e.preventDefault();
+
     let itemDaLista = document.createElement('li');
     itemDaLista.innerHTML = input.value;
-    ulLista.appendChild(itemDaLista)    
+    ulLista.appendChild(itemDaLista);
     itemDaLista.addEventListener('click', marcarTarefa)
+    
+    let btnDeletarTarefa = document.createElement('button');
+    itemDaLista.appendChild(btnDeletarTarefa);
+    btnDeletarTarefa.addEventListener('click', deletarTarefa)
+
+    
+    
+    
+
+
 }
 form.addEventListener('submit', adicionarTarefa);
 

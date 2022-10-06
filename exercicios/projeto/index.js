29ac8f6595e946f52609366cd750b836a600968a
Laylla Farias <layllandradef@live.com>
2022-10-06 14:40:11 -0300

    Resolução da primeira parte da atividade

--- exercicios/projeto/index.js
+++ exercicios/projeto/index.js
@@ -1,17 +1,58 @@
 let inputTarefa = document.querySelector('form input');
 let form = document.querySelector('form');
 let ul = document.getElementById('todo-list-container');
+let botaoMarcarTodos = document.getElementById('marcar-todos')
+let botaoDeletarTodos = document.getElementById('deletar-todos');
 // PEGAR O QUE ESTA ESCRITO NO INPUT
 // console.log(inputTarefa.value)
+
+function deletarTodasTarefas() {
+  let itensDaListaDeTarefas = document.querySelectorAll('li');
+  itensDaListaDeTarefas.forEach(function (li) {
+    // pai.removeChild(filho)
+    ul.removeChild(li);
+  });
+}
+
+botaoDeletarTodos.addEventListener('click', deletarTodasTarefas);
+
+function marcarTodasTarefas() {
+  let itensDaListaDeTarefas = document.querySelectorAll('li');
+  itensDaListaDeTarefas.forEach(function (li) {
+    li.classList.add('checked');
+  })
+}
+
+botaoMarcarTodos.addEventListener('click', marcarTodasTarefas)
+
+function marcarTarefa(evento) {
+  evento.target.classList.toggle('checked');
+}
+
+function deletarTarefa(evento) {
+  // elementoPai.removeChild(filho) ->  filho é o elemento a ser apagado
+  ul.removeChild(evento.target.parentElement);
+}
 
 function cadastrarTarefa(evento) {
   evento.preventDefault();
 
-  let itemDaLista = document.createElement('li');
-  itemDaLista.innerText = inputTarefa.value;
-  // elementoPai.appendChild(elementoFilho) -> elementoFilho é o elemento a ser adicionado
-  ul.appendChild(itemDaLista);
+  if (inputTarefa.value.trim() === '') {
+    alert("Digite uma tarefa")
+  } else {
+    let itemDaLista = document.createElement('li');
+    itemDaLista.innerText = inputTarefa.value;
+    inputTarefa.value = '';
+
+    let botaoDeletar = document.createElement('button');
+    itemDaLista.appendChild(botaoDeletar);
+    botaoDeletar.addEventListener('click', deletarTarefa)
+
+    // elementoPai.appendChild(elementoFilho) -> elementoFilho é o elemento a ser adicionado
+    itemDaLista.addEventListener('click', marcarTarefa)
+    ul.appendChild(itemDaLista);
+  }
 }
 
 // FORM -> SUBMIT (clicar no botao do form ou apertar o enter dentro input) -> cadastrarTarefa(evento)
-form.addEventListener('submit', cadastrarTarefa);
+form.addEventListener('submit', cadastrarTarefa)

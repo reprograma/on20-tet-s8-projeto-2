552fd03fc79aa4588db54ee79fd9e162ae762bcf
Mariana P <mariana.pires.191@gmail.com>
2022-10-06 06:20:56 -0300

    Exercício finalizado. Uma lista de afazeres com os botões 'Marcar/Desmarcar' e 'Limpar'.

--- exercicios/projeto-casa/index.js
+++ exercicios/projeto-casa/index.js
@@ -1,24 +1,83 @@
-//form.addEventListener(`click`, selecionarTarefas)
-//form.addEventListener (`click`, deletarTodasTarefas)
+//VARIÁVEIS
+let escreverTarefa = document.querySelector('form input');
+let semInput = document.querySelector('p');
+let form = document.querySelector('form');
+let botaoMarcarTodos = document.getElementById('button-select')
+let botaoDelete = document.getElementById('button-clean');
+let ul = document.getElementById('todo-list-container');
+let p = document.getElementById('lugar-do-erro');
+//EVENTO PARA CADASTRAR TAREFA
+form.addEventListener('submit', cadastroLista);
 
-ul.appenChild(itemDaLista)
+//FUNCTIONS
+//MARCAR AS TAREFAS
+function marcarTodasTarefas() { 
+        let x = document.getElementById("button-select");
+        if (x.innerHTML === "MARCAR") {
+            let itensDaListaDeTarefas = document.querySelectorAll('li');
+            itensDaListaDeTarefas.forEach(function (li) {
+            li.classList.add('checked');})
+          x.innerHTML = "DESMARCAR";
+        } else {
+          x.innerHTML = "MARCAR";
+          let itensDaListaDeTarefas = document.querySelectorAll('li');
+            itensDaListaDeTarefas.forEach(function (li) {
+            li.classList.remove('checked');})
+        }}
+  
+  botaoMarcarTodos.addEventListener('click', marcarTodasTarefas)
+  function marcarTarefa(evento) {
+    evento.target.classList.toggle('checked');
+  }
 
-function marcarTodasTarefas() {
-    let itensdaLista = document.querySelectorAll(`li`);
+  function toggleTarefa() {
+    let element = document.getElementById("button-select");
+    element.classList.toggle(marcarTodasTarefas);
+ }
 
-    itensdaListaDeTarefa.forEach(function (li) {
-        li.classList.add(`checked`)
+//DELETAR AS TAREFAS
+  function deletarTarefa(evento) {
+    ul.removeChild(evento.target.parentElement);
+    p.removeChild(evento.target.parentElement)
+  }
+
+//CADASTRAR AS TAREFAS
+function cadastroLista (naoAtualizar){
+    naoAtualizar.preventDefault() //não atualizar!
+
+    if (escreverTarefa.value.trim() === ''){
+        let blankPage = document.createElement('content');
+        semInput.value = 'Ué, cadê o texto? 🤔';
+        blankPage.innerText = semInput.value;
+        document.getElementById('lugar-do-erro').appendChild(blankPage);
+    } else {
+        let itemDaLista = document.createElement('li');
+        itemDaLista.innerText = escreverTarefa.value;
+        escreverTarefa.value = '';
+
+        let botaoDeletar = document.createElement('button');
+        itemDaLista.appendChild(botaoDeletar);
+        botaoDeletar.addEventListener('click', deletarTarefa)
+
+        itemDaLista.addEventListener('click', marcarTarefa)
+        ul.appendChild(itemDaLista);
+
+    }
+    
+function deletarTodasTarefas() {
+    //DELETA O ERRO
+    let itensDaListaDeTarefas = document.querySelectorAll('li');
+    itensDaListaDeTarefas.forEach(function (li) {
+      ul.removeChild(li);
     })
+    let ListaDeTarefas = document.querySelectorAll('content');
+    ListaDeTarefas.forEach(function (content) {
+        p.removeChild(content);
+    })
+
+  
 }
+botaoDelete.addEventListener('click', deletarTodasTarefas);
 
-
-marcartarefa(evento){
-evento.targeet.classList.toggle(`checked`);
 
 }
-// let itensdaLista = document.querySelector('li')
-// itensdaLista.forEach(function banana (li) {
-//     li.classList.add('checked')
-// });
-
-// form.addEventListener(`submit`, cadastrarTarefa)

ddad1b3b4a75bf83452a1d774da6e441410e714d
Pati Rocha <patirocha.dev@gmail.com>
2022-10-11 20:28:22 -0300

    Adicionada funcionalidade para verifar tarefas

--- exercicios/projeto-casa/projeto-pati-rocha/script.js
+++ exercicios/projeto-casa/projeto-pati-rocha/script.js
@@ -3,33 +3,59 @@ let input = document.querySelector('input');
 let form = document.querySelector('form');
 let btnForm = document.querySelector('form button');
 let ulLista = document.querySelector('ul');
-let btnMarcarTodas = document.getElementById('btn-marcar-todas');
+let btnMarcarDesmarcarTodas = document.getElementById('btn-marcar-todas');
 let btnDeletarTodas = document.getElementById('btn-deletar-todas');
 let msgErro = document.createElement('p');   
 
+function verificarTarefas() {
+    let todasTarefas = document.querySelectorAll('li');
+    let tarefasMarcadas = document.querySelectorAll('.checked');
+
+    if (todasTarefas && tarefasMarcadas) {
+        if (todasTarefas.length === tarefasMarcadas.length) {
+            btnMarcarDesmarcarTodas.innerText = 'Desmarcar Todas'
+        } else {
+            btnMarcarDesmarcarTodas.innerText = 'Marcar todas'
+        }
+    }    
+}
 function deletarTodas() {
     let todasTarefas = document.querySelectorAll('li');
-    todasTarefas.forEach((itemLi)=>{
-        ulLista.removeChild(itemLi)
-    })
+    todasTarefas.forEach((itemLi) => {
+        ulLista.removeChild(itemLi)        
+    });
+    input.value = '';
+    msgErro.innerText = '';
+    btnMarcarDesmarcarTodas.innerHTML = 'Marcar todas'
 }
 btnDeletarTodas.addEventListener('click', deletarTodas)
 
-function marcarTodas() {
+function marcarDesmarcarTodas() {
     let todasTarefas = document.querySelectorAll('li');
-    todasTarefas.forEach( (itemli) => {
-        let desmarcarTodas = itemli.classList.toggle('marcado')
-        btnMarcarTodas.innerHTML = `${desmarcarTodas ? "Desmarcar todas" : "Marcar todas"}`;
-    })
+
+    if (btnMarcarDesmarcarTodas.innerText === 'Marcar todas') {
+        todasTarefas.forEach((itemli) => {
+            itemli.classList.add('checked')
+        })
+        btnMarcarDesmarcarTodas.innerText = 'Desmarcar todas';
+    } else {
+        todasTarefas.forEach((itemli) => {
+            itemli.classList.remove('checked')
+
+        })
+        btnMarcarDesmarcarTodas.innerText = 'Marcar todas';
 }
-btnMarcarTodas.addEventListener('click',marcarTodas)
+}
+btnMarcarDesmarcarTodas.addEventListener('click',marcarDesmarcarTodas)
 
 function marcarTarefa(e) {
     //e.target é o elemento que recebeu o evento
-    e.target.classList.toggle('marcado');
+    e.target.classList.toggle('checked');
+    verificarTarefas();
 }
 function deletarTarefa(e) {
-   ulLista.removeChild(e.target.parentElement);
+    ulLista.removeChild(e.target.parentElement);   
+    verificarTarefas();
 }
 
 function adicionarTarefa(e) {
@@ -38,10 +64,10 @@ function adicionarTarefa(e) {
     if(input.value.trim() ==='') {
         let main = document.querySelector('main');
         main.insertBefore(msgErro,ulLista)
-        msgErro.innerHTML = '* Texto inválido!'
+        msgErro.innerText = '* Texto inválido!'
     } else {
         
-        msgErro.innerHTML = '';
+        msgErro.innerText = '';
         
         let itemDaLista = document.createElement('li');
         itemDaLista.innerHTML = input.value;
